9bbc5473570d3070db677eedd66915cb5be0e0be
Siddharth Pathak <siddharthpathak415@gmail.com>
2024-07-16 00:18:34 -0700

    LeetCode 242 - Added Dict based solution

--- Python/ValidAnagram.py
+++ Python/ValidAnagram.py
@@ -3,7 +3,27 @@ LeetCode 242 - Valid Anagram
 """
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if ''.join(sorted(s)) == ''.join(sorted(t)):
+        if len(s) != len(t):
+            return False
+
+        count_s = {}
+        count_t = {}
+
+        for char in s:
+            if char in count_s:
+                count_s[char] += 1
+            else:
+                count_s[char] = 1
+        
+        for char in t:
+            if char in count_t:
+                count_t[char] += 1
+            else:
+                count_t[char] = 1
+        
+        return count_s == count_t
+        """if ''.join(sorted(s)) == ''.join(sorted(t)):
             return True
 
         return False
+        """
